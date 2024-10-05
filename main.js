446896abbc4f01d369bfde348bcbcde0661aca03
Augusto Berloin <nfunes706@gmail.com>
2024-10-05 17:31:46 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,26 @@
+//variables
+
+let nombre = "e"; // let nos sirve para los bloques de código
+var nombre2 ="Juan"; // var es como una variable global
+
+const nombre3 ="Carlos";
+
+/*
+ valores
+
+ boolean = true/false
+ string = "cadena"
+ number =0
+ null -> este no reserva un espacio en memoria
+ undefined -> este sí
+
+
+ */
+
+/*
+        Operadores
+
+        
+ */
+
+
